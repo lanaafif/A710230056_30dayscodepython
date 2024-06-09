0e659301ab1973af5457f6708f40985c82f40bc6
lanaafif <meilanaafif@gmail.com>
2024-06-09 12:42:07 +0700

    Create upperdanlower.py

---
+++ day 4/upperdanlower.py
@@ -0,0 +1,11 @@
+#kata asli
+kata = 'Selamat datang di Prodi PTI UMS'
+
+#ubah menjadi kapital dan kecil
+kapital = kata.upper()
+kecil = kata.lower()
+
+#cetak kata
+print(kata)
+print(kapital)
+print(kecil)
