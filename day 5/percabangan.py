eebc542a44f078daa5ca5e1042aace30b2cf4758
lanaafif <meilanaafif@gmail.com>
2024-06-10 20:37:35 +0700

    Create percabangan.py

---
+++ day 5/percabangan.py
@@ -0,0 +1,10 @@
+nilai = 80
+
+if nilai > 90:
+    print('Kamu Lulus Beasiswa Penuh')
+elif nilai >=80 :
+    print('Kamu Mendapat Beasiswa 1')
+elif nilai >=70 :
+    print('Kamu Mendapat Beasiswa 2')
+else:
+    print('Tidak Mendapat Beasiswa')
