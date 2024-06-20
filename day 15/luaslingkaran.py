a2257acc30a646d69da1123f556aabcda9e7d340
lanaafif <meilanaafif@gmail.com>
2024-06-20 20:12:32 +0700

    Create luaslingkaran.py

---
+++ day 15/luaslingkaran.py
@@ -0,0 +1,13 @@
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+ 
+    def calculate_area(self):
+        area = 3.14 * self.radius ** 2
+        return area
+ 
+# Creating an instance of Circle
+circle_instance = Circle(5)
+ 
+# Calling the calculate_area method
+print("Area of the circle:", circle_instance.calculate_area())
