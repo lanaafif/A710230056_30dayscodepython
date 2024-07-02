bdaafa8f6ae7bc647c9d580931c2c55d696a9afe
lanaafif <meilanaafif@gmail.com>
2024-07-02 18:47:14 +0000

    Create bilangankuadrat.py

---
+++ day27/bilangankuadrat.py
@@ -0,0 +1,6 @@
+try:
+    bilangan = int(input("Masukkan bilangan bulat: "))
+    kuadrat = bilangan ** 2
+    print(f"Kuadrat dari {bilangan} adalah {kuadrat}")
+except:
+    print("Input yang dimasukkan tidak valid!")
